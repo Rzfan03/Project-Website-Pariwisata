181b57078db928ac2bd0471c03f33f864759f0c8
Rzfan03 <fajrulsyaban47@gmail.com>
2025-11-02 05:49:59 +0700

    dan yap sudah done tinggal responsive

--- src/js/main.js
+++ src/js/main.js
@@ -2,26 +2,48 @@ $(window).scroll(function() {
   if ($(document).scrollTop() > 50) {
     $('#navbarv').addClass('!bg-white shadow-lg');
     $('#navbarv').removeClass('bg-gradient-to-b from-black/50 to-transparent');
+    
     $('#zalando li a').addClass('text-gray-700');
     $('#zalando li a').removeClass('text-white');
     $('#title-nav').removeClass('text-white');
     
     $('#dropdownNavbarLink').addClass('text-gray-700');
     $('#dropdownNavbarLink').removeClass('text-white');
-    $('#dropdownNavbar a').addClass('text-gray-700');
-    $('#dropdownNavbar a').removeClass('text-white');
+    $('#dropdownNavbar a').addClass('text-gray-950');
+
     
     console.log("OK");
   } else {
     $('#navbarv').removeClass('!bg-white shadow-lg');
     $('#navbarv').addClass('bg-gradient-to-b from-black/50 to-transparent');
+    
     $('#zalando li a').addClass('text-white');
     $('#zalando li a').removeClass('text-gray-700');
     $('#title-nav').addClass('text-white');
     
     $('#dropdownNavbarLink').addClass('text-white');
     $('#dropdownNavbarLink').removeClass('text-gray-700');
-    $('#dropdownNavbar a').addClass('text-white');
-    $('#dropdownNavbar a').removeClass('text-gray-700');
   }
+});
+
+// function faq
+document.addEventListener('DOMContentLoaded', function() {
+  const faqQuestions = document.querySelectorAll('.faq-question');
+  
+  faqQuestions.forEach(question => {
+    question.addEventListener('click', function() {
+      const answer = this.nextElementSibling;
+      const icon = this.querySelector('svg');
+      
+      this.classList.toggle('active');
+      answer.classList.toggle('show');
+      
+      faqQuestions.forEach(otherQuestion => {
+        if (otherQuestion !== this) {
+          otherQuestion.classList.remove('active');
+          otherQuestion.nextElementSibling.classList.remove('show');
+        }
+      });
+    });
+  });
 });
