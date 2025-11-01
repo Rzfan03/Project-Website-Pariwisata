e07082455765a81e0cdeb20385551c4420139cc0
Rzfan03 <fajrulsyaban47@gmail.com>
2025-11-01 19:48:17 +0700

    masi banyak bug but its okey

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,27 @@
 $(window).scroll(function() {
   if ($(document).scrollTop() > 50) {
-      $('#navbarv').addClass('!bg-white shadow-lg');
-      $('#navbarv').removeClass('bg-gradient-to-b from-black/50 to-transparent');
-      $('#zalando li a').addClass('text-gray-700');
-      $('#zalando li a').removeClass('text-white');
-      $('#title-nav').removeClass('text-white');
-      console.log("OK");
-    } else {
-      $('#navbarv').removeClass('!bg-white bg-gradient-to-b from-black/50 to-transparent shadow-lg');
-      $('#navbarv').addClass('bg-gradient-to-b from-black/50 to-transparent');
-      $('#zalando li a').addClass('text-white');
-      $('#zalando li a').removeClass('text-gray-700');
-      $('#title-nav').addClass('text-white');
+    $('#navbarv').addClass('!bg-white shadow-lg');
+    $('#navbarv').removeClass('bg-gradient-to-b from-black/50 to-transparent');
+    $('#zalando li a').addClass('text-gray-700');
+    $('#zalando li a').removeClass('text-white');
+    $('#title-nav').removeClass('text-white');
+    
+    $('#dropdownNavbarLink').addClass('text-gray-700');
+    $('#dropdownNavbarLink').removeClass('text-white');
+    $('#dropdownNavbar a').addClass('text-gray-700');
+    $('#dropdownNavbar a').removeClass('text-white');
+    
+    console.log("OK");
+  } else {
+    $('#navbarv').removeClass('!bg-white shadow-lg');
+    $('#navbarv').addClass('bg-gradient-to-b from-black/50 to-transparent');
+    $('#zalando li a').addClass('text-white');
+    $('#zalando li a').removeClass('text-gray-700');
+    $('#title-nav').addClass('text-white');
+    
+    $('#dropdownNavbarLink').addClass('text-white');
+    $('#dropdownNavbarLink').removeClass('text-gray-700');
+    $('#dropdownNavbar a').addClass('text-white');
+    $('#dropdownNavbar a').removeClass('text-gray-700');
   }
 });
