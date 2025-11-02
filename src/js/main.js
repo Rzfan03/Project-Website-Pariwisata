d9cdb7819321969a78ef50b9926213be4e3c27eb
SyauqiAlifI <vstudio776@gmail.com>
2025-11-02 09:51:14 +0800

    responsive

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@ $(window).scroll(function() {
     $('#navbarv').addClass('!bg-white shadow-lg');
     $('#navbarv').removeClass('bg-gradient-to-b from-black/50 to-transparent');
     
+    $('#zalando').removeClass('bg-black/60');
     $('#zalando li a').addClass('text-gray-700');
     $('#zalando li a').removeClass('text-white');
     $('#title-nav').removeClass('text-white');
@@ -17,6 +18,7 @@ $(window).scroll(function() {
     $('#navbarv').removeClass('!bg-white shadow-lg');
     $('#navbarv').addClass('bg-gradient-to-b from-black/50 to-transparent');
     
+    $('#zalando').addClass('bg-black/60');
     $('#zalando li a').addClass('text-white');
     $('#zalando li a').removeClass('text-gray-700');
     $('#title-nav').addClass('text-white');
